ba4f0a47d51255fe89577d15daff16f66f8c8d4f
Robin Reyniers <Robinreyniers@hotmail.com>
2022-10-18 18:47:18 +0200

    push

---
+++ 2. Vanilla Javascript/intro/index.js
@@ -0,0 +1,45 @@
+// const items = ["id1", "id2"];
+// let text = object.freeze("");
+// let number = 1;
+// let boolean = false;
+
+// let array = ["id1", "id2"];
+// let object = {};
+
+// let reassigned;
+
+// reassigned = [...array];
+
+// reassigned.push("id3");
+
+// console.log(array);
+// console.log(reassigned);
+
+// let customMap = function map(array, mapper) {
+//   const newArray = [];
+
+//   for (const item of array) {
+//     newArray.push(mapper(item));
+//   }
+
+//   return newArray;
+// };
+
+// const mapper = (item) => item * 2;
+// const array = [1, 2, 3, 4];
+// console.log();
+
+let customFilter = (array, filteredItem) {
+  let newArray = [];
+
+  for (const item of array) {
+    
+  }
+
+  return newArray;
+};
+
+const filteredItem = (item) => item > 2;
+const array = [1, 2, 3, 4];
+
+console.log(customFilter);
