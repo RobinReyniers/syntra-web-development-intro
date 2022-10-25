1529f3202b4607685e035cdbe7d7c84f775f05b2
Robin Reyniers <Robinreyniers@hotmail.com>
2022-10-25 09:50:01 +0200

    commit

--- 1. Vanilla Javascript/32 - Fun with Promises/promises.js
+++ 1. Vanilla Javascript/32 - Fun with Promises/promises.js
@@ -1,184 +1,237 @@
-/**
- * What is the output of the code snippet?
- */
-{
-  const promise = new Promise((resolve, reject) => {
-    resolve("success1");
-    reject("fail");
-    resolve("success2");
+// /**
+//  * What is the output of the code snippet?
+//  */
+
+// {
+//   const promise = new Promise((resolve, reject) => {
+//     resolve("success1");
+//     reject("fail");
+//     resolve("success2");
+//   });
+
+//   promise
+//     .then((res) => {
+//       console.log("then: ", res);
+//     })
+//     .catch((err) => {
+//       console.log("catch: ", err);
+//     });
+// }
+
+// /**
+//  * What is the output of the code snippet?
+//  */
+// {
+//   const promise = new Promise((resolve, reject) => {
+//     reject("fail");
+//     resolve("success2");
+//   });
+//   promise
+//     .then((res) => {
+//       console.log("then1: ", res);
+//     })
+//     .then((res) => {
+//       console.log("then2: ", res);
+//     })
+//     .catch((err) => {
+//       console.log("catch: ", err);
+//     })
+//     .then((res) => {
+//       console.log("then3: ", res);
+//     });
+// }
+
+// /**
+//  * What is the output of this code snippet?
+//  */
+// {
+//   Promise.resolve(1)
+//     .then((res) => {
+//       console.log(res);
+//       return 2;
+//     })
+//     .catch((err) => {
+//       return 3;
+//     })
+//     .then((res) => {
+//       console.log(res);
+//     })
+//     .then((res) => {
+//       console.log(res);
+//     });
+// }
+
+// /**
+//  * What is the output of this code snippet?
+//  */
+// {
+//   Promise.reject(1)
+//     .then((res) => {
+//       console.log(res);
+//       return 2;
+//     })
+//     .catch((err) => {
+//       return 3;
+//     })
+//     .then((res) => {
+//       console.log(res);
+//     })
+//     .then((res) => {
+//       console.log(res);
+//     });
+// }
+
+// /**
+//  * What is the output of this code snippet?
+//  */
+// {
+//   const promise = new Promise((resolve, reject) => {
+//     resolve(1);
+//   });
+
+//   promise.then((res) => {
+//     console.log("first then: ", res);
+//     return 2;
+//   });
+
+//   promise.then((res) => {
+//     console.log("second then: ", res);
+//     return 3;
+//   });
+
+//   promise.then((res) => {
+//     console.log("third then: ", res);
+//   });
+// }
+
+// /**
+//  * What is the output of this snippet?
+//  */
+// {
+//   Promise.resolve()
+//     .then(() => {
+//       return new Error("error!!!");
+//     })
+//     .then((res) => {
+//       console.log("then: ", res);
+//     })
+//     .catch((err) => {
+//       console.log("catch: ", err);
+//     });
+// }
+
+// /**
+//  * What is the output of this snippet?
+//  */
+// {
+//   Promise.resolve(1).then(2).then(Promise.resolve(3)).then(console.log);
+// }
+
+// /**
+//  * Promisify the following function
+//  */
+// function loadScript(src, callback) {
+//   let script = document.createElement("script");
+//   script.src = src;
+
+//   script.onload = () => callback(null, script);
+//   script.onerror = () => callback(new Error(`Script load error for ${src}`));
+
+//   document.head.append(script);
+// }
+
+// const waitForMove = (element, transform) => {
+//   element.addEventListener("transitionend", () => console.log("done"), {
+//     once: true,
+//   });
+//   element.style.transform = transform;
+// };
+
+// const ball = document.getElementById("ball");
+// // await waitForMove(ball, "translate(10px)");
+
+// /**
+//  * Let's do some practice with a simple exercice. You must modify the code below based on the following rules:
+//     1. The function job must return a promise object (you are in a NodeJS environment, you can use new Promise)
+//     2. The promise must resolve itself 2 seconds after the call to job and must provide hello world in the data
+//  */
+
+// // function job() {
+// //   return new Promise((resolve, reject) => {
+// //     setTimeout(() => {
+// //       resolve("Hello World");
+// //     });
+// //   }, 2000);
+// // }
+// // job().then((blah) => console.log(blah));
+
+// /**
+//  * Let's do a harder exercise. In this code, your function receives a parameter data. You must modify the code below based on the following rules:
+
+//     1. Your function must always return a promise
+//     2. If data is not a number, return a promise rejected instantly and give the data "error" (in a string)
+//     3. If data is an odd number, return a promise resolved 1 second later and give the data "odd" (in a string)
+//     4. If data is an even number, return a promise rejected 2 seconds later and give the data "even" (in a string)
+//  */
+
+// // function job(data) {
+// //   return new Promise((resolve, reject) => {
+// //     if (isNaN(data)) {
+// //       reject("error");
+// //     } else if (data % 2 === 0) {
+// //       setTimeout(() => {
+// //         reject("even");
+// //       }, 2000);
+// //     } else {
+// //       setTimeout(() => {
+// //         resolve("odd");
+// //       }, 1000);
+// //     }
+// //   });
+// // }
+// // job("poop")
+// //   .then((v) => console.log(v))
+// //   .catch((v) => console.log(v));
+// /**
+//  * Build a guessing game:
+//  *  1. A user can enter a number
+//     2. The system picks a random number from 1 to 6
+//     3. If the user's number is equal to a random number, give the user 2 points
+//     4. If the user's number is different than the random number by 1, give the user 1 point. Otherwise, give the user 0 points
+//     5. The user can play the game as long as they want to
+//  */
+
+const enterNumber = () => {
+  return new Promise((resolve, reject) => {
+    let userInput = prompt("Enter a number. (1 - 6)");
+    let randomNumber = Math.floor(Math.random() * 6);
+    if (parseInt(userInput) === randomNumber) {
+      resolve("You get + 2 points");
+    }
+    if (parseInt(userInput) === randomNumber + 1) {
+      resolve("You get + 1 points");
+    }
+    if (parseInt(userInput) === randomNumber - 1) {
+      resolve("You get + 1 points");
+    }
+
+    resolve("You get 0 points");
   });
-
-  promise
-    .then((res) => {
-      console.log("then: ", res);
-    })
-    .catch((err) => {
-      console.log("catch: ", err);
-    });
-}
-
-/**
- * What is the output of the code snippet?
- */
-{
-  const promise = new Promise((resolve, reject) => {
-    reject("fail");
-    resolve("success2");
-  });
-  promise
-    .then((res) => {
-      console.log("then1: ", res);
-    })
-    .then((res) => {
-      console.log("then2: ", res);
-    })
-    .catch((err) => {
-      console.log("catch: ", err);
-    })
-    .then((res) => {
-      console.log("then3: ", res);
-    });
-}
-
-/**
- * What is the output of this code snippet?
- */
-{
-  Promise.resolve(1)
-    .then((res) => {
-      console.log(res);
-      return 2;
-    })
-    .catch((err) => {
-      return 3;
-    })
-    .then((res) => {
-      console.log(res);
-    })
-    .then((res) => {
-      console.log(res);
-    });
-}
-
-/**
- * What is the output of this code snippet?
- */
-{
-  Promise.reject(1)
-    .then((res) => {
-      console.log(res);
-      return 2;
-    })
-    .catch((err) => {
-      return 3;
-    })
-    .then((res) => {
-      console.log(res);
-    })
-    .then((res) => {
-      console.log(res);
-    });
-}
-
-/**
- * What is the output of this code snippet?
- */
-{
-  const promise = new Promise((resolve, reject) => {
-    resolve(1);
-  });
-
-  promise.then((res) => {
-    console.log("first then: ", res);
-    return 2;
-  });
-
-  promise.then((res) => {
-    console.log("second then: ", res);
-    return 3;
-  });
-
-  promise.then((res) => {
-    console.log("third then: ", res);
-  });
-}
-
-/**
- * What is the output of this snippet?
- */
-{
-  Promise.resolve()
-    .then(() => {
-      return new Error("error!!!");
-    })
-    .then((res) => {
-      console.log("then: ", res);
-    })
-    .catch((err) => {
-      console.log("catch: ", err);
-    });
-}
-
-/**
- * What is the output of this snippet?
- */
-{
-  Promise.resolve(1).then(2).then(Promise.resolve(3)).then(console.log);
-}
-
-/**
- * Promisify the following function
- */
-function loadScript(src, callback) {
-  let script = document.createElement("script");
-  script.src = src;
-
-  script.onload = () => callback(null, script);
-  script.onerror = () => callback(new Error(`Script load error for ${src}`));
-
-  document.head.append(script);
-}
-
-const waitForMove = (element, transform) => {
-  element.addEventListener("transitionend", () => console.log("done"), { once: true });
-  element.style.transform = transform;
 };
 
-const ball = document.getElementById("ball");
-await waitForMove(ball, "translate(10px)");
+enterNumber().then((v) => {
+  alert(v);
+});
 
-/**
- * Let's do some practice with a simple exercice. You must modify the code below based on the following rules:
-    1. The function job must return a promise object (you are in a NodeJS environment, you can use new Promise)
-    2. The promise must resolve itself 2 seconds after the call to job and must provide hello world in the data
- */
-
-function job() {
-  return "hello world";
-}
-
-/**
- * Let's do a harder exercise. In this code, your function receives a parameter data. You must modify the code below based on the following rules:
-
-    1. Your function must always return a promise
-    2. If data is not a number, return a promise rejected instantly and give the data "error" (in a string)
-    3. If data is an odd number, return a promise resolved 1 second later and give the data "odd" (in a string)
-    4. If data is an even number, return a promise rejected 2 seconds later and give the data "even" (in a string)
- */
-
-function job(data) {
-  return something;
-}
-
-/**
- * Build a guessing game:
- *  1. A user can enter a number
-    2. The system picks a random number from 1 to 6
-    3. If the user's number is equal to a random number, give the user 2 points
-    4. If the user's number is different than the random number by 1, give the user 1 point. Otherwise, give the user 0 points
-    5. The user can play the game as long as they want to
- */
-
-const enterNumber = () => {};
-const continueGame = () => {};
+const continueGame = () => {
+  return new Promise((resolve, reject) => {
+    let playAgain = prompt("Do you want to play again?");
+    if (playAgain) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+};
+continueGame().then(enterNumber());
 const handleGuess = () => {};
